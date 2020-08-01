4e9fcb42ff0627219dd5057f332de43340779950
lesea16 <codru1993@gmail.com>
2020-08-01 10:34:20 -0500

    add HappyB20.java

--- src/HelloB20.java
+++ src/HelloB20.java
@@ -2,6 +2,10 @@ public class HelloB20 {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello B20");
+        System.out.println("Hello B20, it's my pleasure to be with you!");
+
+
+
+        //added a line
     }
 }
